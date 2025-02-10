031f680731efee4a434caab4bcf425fb0c872c20
Andre da Silva <andre.dasilva@linera.io>
2025-02-10 12:42:42 -0300

    Move listen_for_shutdown_signals to linera-base

--- linera-base/src/lib.rs
+++ linera-base/src/lib.rs
@@ -10,8 +10,11 @@
 
 use std::fmt;
 
+use ::tracing::debug;
 #[doc(hidden)]
 pub use async_trait::async_trait;
+use tokio::signal::unix;
+use tokio_util::sync::CancellationToken;
 pub mod abi;
 #[cfg(not(target_arch = "wasm32"))]
 pub mod command;
@@ -146,4 +149,23 @@ pub fn hex_vec_debug(list: &Vec<Vec<u8>>, f: &mut fmt::Formatter) -> fmt::Result
         hex_debug(bytes, f)?;
     }
     write!(f, "]")
+}
+
+/// Listens for shutdown signals, and notifies the [`CancellationToken`] if one is
+/// received.
+pub async fn listen_for_shutdown_signals(shutdown_sender: CancellationToken) {
+    let _shutdown_guard = shutdown_sender.drop_guard();
+
+    let mut sigint =
+        unix::signal(unix::SignalKind::interrupt()).expect("Failed to set up SIGINT handler");
+    let mut sigterm =
+        unix::signal(unix::SignalKind::terminate()).expect("Failed to set up SIGTERM handler");
+    let mut sighup =
+        unix::signal(unix::SignalKind::hangup()).expect("Failed to set up SIGHUP handler");
+
+    tokio::select! {
+        _ = sigint.recv() => debug!("Received SIGINT"),
+        _ = sigterm.recv() => debug!("Received SIGTERM"),
+        _ = sighup.recv() => debug!("Received SIGHUP"),
+    }
 }
